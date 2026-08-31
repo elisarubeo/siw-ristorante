973bc7ad818fb4cd589c8a01f39ac5face6604e7
Elisa Rubeo <rubeo.elisa4@gmail.com>
2026-08-31 19:14:34 +0200

    init

---
+++ src/test/java/it/uniroma3/siw_ristorante/SiwRistoranteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw_ristorante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwRistoranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
